c21aa030917b2411ef6eef5d923c9763cc34c5d9
Tkakishak <tkakishakd@mail.ru>
2023-03-22 23:32:04 +0300

    question

--- src/task1/Book.java
+++ src/task1/Book.java
@@ -1,28 +1,33 @@
 package task1;
 
+import task1.Author;
+
 public class Book {
 
     private final String nameBook;
     private int yearCreate;
+    private final Author author;
 
+    public Book(Author author, String nameBook, int yearCreate) {
+        this.nameBook = nameBook;
+        this.yearCreate = yearCreate;
+        this.author = author;
 
-
- public Book (String nameBook, int yearCreate) {
-     this.nameBook = nameBook;
-     this.yearCreate = yearCreate;
-
-
- }
-
-
- public String getNameBook () {
-     return this.nameBook;
- }
+    }
 
     public int getYearCreate() {
-     return yearCreate;
+        return yearCreate;
     }
-    public void setYearCreate (int yearCreate) {
-     this.yearCreate = yearCreate;
+
+    public void setYearCreate(int yearCreate) {
+        this.yearCreate = yearCreate;
+    }
+
+    public String getNameBook() {
+        return nameBook;
+    }
+
+    public Author getAuthor() {
+        return author;
     }
 }
--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,15 +1,11 @@
 package task1;
 
+
 public class Main {
     public static void main(String[] args) {
-        Author authorBook1 = new Author("Пушкин ", "Александр Сергеевич ");
-        Book book1 = new Book("Капитанская дочка ",1836);
-        System.out.println(authorBook1.getFirstNameAuthor()+authorBook1.getNameAuthor()+book1.getNameBook()+book1.getYearCreate());
-        Author authorBook2 = new Author("Литвак ", "Михаил Ефимович ");
-        Book book2 = new Book("Психологическое айкидо ", 2025);
-        System.out.println(authorBook2.getFirstNameAuthor()+authorBook2.getNameAuthor()+book2.getNameBook()+book2.getYearCreate());
-        book2.setYearCreate(2011);
-        System.out.println(authorBook2.getFirstNameAuthor()+authorBook2.getNameAuthor()+book2.getNameBook()+ book2.getYearCreate());
+        Book book1 = new Book("Пушкин ","Капитанская дочка ",1836);
+        System.out.println(book1.getAuthor()+book1.getNameBook()+book1.getYearCreate());
+
     }
 
 }
