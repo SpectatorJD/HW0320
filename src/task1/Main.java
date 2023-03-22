cee160dcfcee8952553031116540a2c3370cad66
d.tkakishak <Tkakishakd@mail.ru>
2023-03-22 17:19:02 +0300

    HW1/2

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -3,8 +3,11 @@ package task1;
 public class Main {
     public static void main(String[] args) {
         Book book1 = new Book  ("Пушкин ", "Александр Сергеевич " , "Капитанская дочка ", 1836);
-        System.out.println(book1.getFirstNameAuthor());
-
+        System.out.println(book1.getFirstNameAuthor() + book1.getNameAuthor()+ book1.getNameBook()+ book1.getYearCreate());
+        Book book2 = new Book("Литвак ", "Михаил ", "Психология ", 2025);
+        System.out.println(book2.getFirstNameAuthor()+book1.getNameAuthor()+ book2.getNameBook()+ book2.getYearCreate());
+        book2.setYearCreate(2011);
+        System.out.println(book2.getFirstNameAuthor()+book1.getNameAuthor()+ book2.getNameBook()+ book2.getYearCreate());
     }
 
 }
